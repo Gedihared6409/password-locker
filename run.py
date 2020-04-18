742c2dd561b08b72711584e136e1710c003b8da6
Gedihared6409 <gediali019@gmail.com>
2020-04-18 13:39:41 +0300

    created  Function to save user account

--- run.py
+++ run.py
@@ -5,6 +5,9 @@ from credentials import credentials
 
 
 def create_user_account(user_name,pass_word):
-    new_user = user(user_name,pass_word)
+    new_user = User(user_name,pass_word)
     return new_user
-    
+
+def save_user_account(user):
+    user.save_user()
+
