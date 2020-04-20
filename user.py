8547c0bea4556bb345047d0e2c4a367f97807214
Gedihared6409 <gediali019@gmail.com>
2020-04-20 07:29:59 +0300

    added function that create ,delete and display credential

--- user.py
+++ user.py
@@ -25,6 +25,40 @@ class User:
                 return True
 
         return False
+class Credentials:
+
+    credentials_list = []
+    def __init__(self,view_passward,account,login,passward):
+        self.view_passward = view_passward
+        self.account = account
+        self.login = login
+        self.passward = passward
+
+
+
+    def save_credential(self):
+        Credentials.credentials_list.append(self)
+
+    def del_credential(self):
+        Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def display_credentials(cls):
+        return cls.credentials_list
+        
+    @classmethod
+    def find_by_account(cls,account):
+        for creden in cls.credentials_list:
+            if creden.account == account:
+                return creden
+        return False
+
+    @classmethod
+    def credentials_exist(cls,view_passward):
+        for creden in cls.credentials_list:
+            if creden.view_passward == view_passward:
+                return True
+        return False
 
 
         
